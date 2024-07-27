baa6cdec1da5ed7f4592650b38c6586520f89b2b
Olukayode Ogunnowo <o.ogunnowo@native.semicolon.africa>
2024-07-27 18:45:51 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    gridTemplateColumns: {
+      auto: "repeat(auto-Fit, minmax(200px, 1fr))",
+    },
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+      Ovo: ["Ovo", "serif"],
+    },
+    animation: {
+      spin_slowly: "spin 6s linear infinite",
+    },
+  },
+};
